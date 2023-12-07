baea09f2b1dcbd86a4fbe300535d879fad5508bf
Dave Maynard <dmayna@amazon.com>
2023-12-07 01:06:43 -0500

    add new count badge

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer: React.FC = () => {
   return (
-    <nav className="pt-4 mt-8 border-t-slate-200 border-t-2">
+    <nav className="pt-4 mt-8 border-t-white border-opacity-30 border-t-2">
       <small>&copy; 2023 DaveCo</small>
     </nav>
   );
